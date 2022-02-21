35e7422f28cb1cc464c5b2f9e11c46a15a9887f8
Ridwan Ajibola <51078424+rajibola@users.noreply.github.com>
2022-02-22 00:27:44 +0100

    chore: Changed home colors

--- src/pages/home/hero/index.tsx
+++ src/pages/home/hero/index.tsx
@@ -63,23 +63,22 @@ const Hero = () => {
   }, []);
 
   return (
-    <div className="w-screen overflow-hidden relative h-screen flex">
+    <div className="w-screen overflow-hidden relative h-screen flex bg-black">
+      <Header show={true} />
       <div ref={cover}>
-        <div className="absolute w-full h-full bg-[#FFF2C5] z-20 top-0" />
-        <div className="absolute w-full h-full bg-[#FFF2C5] z-20 left-[33.3%] top-0" />
-        <div className="absolute w-full h-full bg-[#FFF2C5] z-20 left-[66.6%] top-0" />
+        <div className="absolute w-full h-full bg-[#838E83] z-40 top-0" />
+        <div className="absolute w-full h-full bg-[#838E83] z-40 left-[33.3%] top-0" />
+        <div className="absolute w-full h-full bg-[#838E83] z-40 left-[66.6%] top-0" />
       </div>
 
-      <Header show={true} />
-
-      <div className="w-[425px] top-0 bg-[#F0D0AA] left-[420px] bottom-[290px] flex justify-center gap-8 absolute -z-10">
+      <div className="w-[425px] top-0 bg-[#838E83] left-[420px] bottom-[290px] flex justify-center gap-8 absolute z-10">
         <Link to="#">in</Link>
         <Link to="#">fb</Link>
         <Link to="#">gh</Link>
 
         <div
           ref={myText}
-          className="w-[406px] absolute bottom-[80px] -right-[370px] font-sourceSansPro z-10"
+          className="w-[406px] absolute bottom-[80px] -right-[370px] font-sourceSansPro z-10 text-white"
         >
           <div className="font-bold text-4xl mb-4">
             react-native/react developer.
@@ -96,21 +95,30 @@ const Hero = () => {
           </Link>
         </div>
       </div>
-      <div className="w-full h-full mt-[145px] absolute ml-[112px] bg-[#FFF2C5] -z-20 flex"></div>
+      <div className="w-full h-full mt-[145px] absolute ml-[112px] bg-[#357777] z-0 flex" />
       <div ref={ridwan} className="w-min h-min absolute left-14 bottom-[120px]">
         <LargeText>RID</LargeText>
         <LargeText>WAN</LargeText>
       </div>
 
-      <div className="h-[140px] w-full pl-[532px] self-end">
-        <div className="bg-white w-full h-full flex items-center justify-center gap-40">
-          <Link to="#" className="border-b-2 pb-3 border-black leading-none">
+      <div className="h-[140px] w-full pl-[532px] self-end z-10">
+        <div className="bg-black w-full h-full flex items-center justify-center gap-40">
+          <Link
+            to="#"
+            className="border-b-2 pb-3 border-white leading-none text-white"
+          >
             download cv
           </Link>
-          <Link to="#" className="border-b-2 pb-3 border-black leading-none">
+          <Link
+            to="#"
+            className="border-b-2 pb-3 border-white leading-none text-white"
+          >
             latest work
           </Link>
-          <Link to="#" className="border-b-2 pb-3 border-black leading-none">
+          <Link
+            to="#"
+            className="border-b-2 pb-3 border-white leading-none text-white"
+          >
             case studies
           </Link>
         </div>
@@ -125,7 +133,7 @@ const LargeText = styled.h1`
   ${tw`text-[152px] font-bold font-robotoCondensed h-[135px] overflow-hidden`}
   color: transparent;
   -webkit-text-stroke: 1px;
-  -webkit-text-stroke-color: black;
+  -webkit-text-stroke-color: #ffffff;
   -webkit-text-fill-color: transparent;
   line-height: 135px;
 `;
