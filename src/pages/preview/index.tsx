80b45ee0c118aab6807cba07f843ff79e24a889a
Ridwan Ajibola <51078424+rajibola@users.noreply.github.com>
2022-03-03 09:08:17 +0100

    feat: Added parallax for image display for project

--- src/pages/preview/index.tsx
+++ src/pages/preview/index.tsx
@@ -2,12 +2,14 @@ import ScrollContainer from "shared/scroll-container";
 import SmoothScroll from "shared/smooth-scroll";
 import { Hero } from "./hero";
 import { Info } from "./info";
+import { Showcase } from "./showcase";
 
 export const Preview = () => {
   return (
-    <div>
+    <>
       <Hero />
       <Info />
-    </div>
+      <Showcase />
+    </>
   );
 };
---
+++ src/pages/preview/showcase/index.tsx
@@ -0,0 +1,46 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useEffect, useRef } from "react";
+gsap.registerPlugin(ScrollTrigger);
+
+export const Showcase = () => {
+  const screenRef = useRef<HTMLDivElement[]>([]);
+  useEffect(() => {
+    gsap.utils.toArray(screenRef.current).forEach((section: any) => {
+      ScrollTrigger.create({
+        trigger: section,
+        start: "top top",
+        pin: true,
+        pinSpacing: false,
+        scrub: 1,
+        markers: true,
+      });
+    });
+  }, []);
+
+  const addToRef = (el: any) =>
+    el && !screenRef.current.includes(el) && screenRef.current.push(el);
+
+  return (
+    <section className="relative">
+      <div
+        ref={addToRef}
+        className="w-full h-screen sticky flex items-center justify-center bg-red-500"
+      >
+        <h1 className="text-22">HHEHEHEH</h1>
+      </div>
+      <div
+        ref={addToRef}
+        className="w-full h-screen sticky flex items-center justify-center bg-lime-500"
+      >
+        <h1 className="text-22">HHEHEHEH</h1>
+      </div>
+      <div
+        ref={addToRef}
+        className="w-full h-screen sticky flex items-center justify-center bg-sky-500"
+      >
+        <h1 className="text-22">HHEHEHEH</h1>
+      </div>
+    </section>
+  );
+};
