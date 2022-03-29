7d4c25325a30bb74bdd89b5b5f4cc66850c04358
Ridwan Ajibola <51078424+rajibola@users.noreply.github.com>
2022-03-29 07:33:48 +0100

    fix: Image sizes

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -2,7 +2,7 @@ export { default as nasa } from "assets/nasa.jpeg";
 export { default as web } from "assets/web.png";
 export { default as portfolio } from "assets/portfolio1.webp";
 export { default as pexel } from "assets/pexel.jpeg";
-export { default as dark } from "assets/dark.png";
+export { default as dark } from "assets/dark.webp";
 export { default as arrow } from "assets/arrow-top.svg";
 export { default as project1 } from "assets/project1.png";
 export { default as project2 } from "assets/details-1.png";
