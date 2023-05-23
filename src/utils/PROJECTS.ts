62cac765f190eee03ef467d1ed446c999af8f4d3
Ridwan Ajibola <51078424+rajibola@users.noreply.github.com>
2023-05-23 11:23:48 +0100

    feat: Added BannerWave projects

--- src/utils/PROJECTS.ts
+++ src/utils/PROJECTS.ts
@@ -20,6 +20,58 @@ const PROJECTS: {
   tag: "web" | "mobile";
 }[] = [
   {
+    name: "Animere",
+    title: "A Web Banner Ad Designer",
+    tools: [
+      "Next13",
+      "TypeScript",
+      "Redux",
+      "Framer Motion",
+      "Styled-Component",
+      "GraphQL",
+      "Apollo",
+    ],
+    images: [
+      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684825840/portfolio/screenshot-2023-05-21-at-173739_mo61rx.webp",
+      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684825840/portfolio/screenshot-2023-05-21-at-174512_sof3e3.webp",
+      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684825840/portfolio/screenshot-2023-05-21-at-174442_a5j1wz.webp",
+      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684825841/portfolio/screenshot-2023-05-21-at-174618_stpsk9.webp",
+      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684825840/portfolio/screenshot-2023-04-18-at-225233_oysma3.webp",
+      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684825840/portfolio/screenshot-2023-05-21-at-173953_awtpk1.webp",
+      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684825841/portfolio/screenshot-2023-05-21-at-174546_zmnm2k.webp",
+    ],
+    details:
+      "We’ve worked incredibly hard to build a talented, industry leading team of professionals. With a team of creative, strategist, business and development specialists, we consistently strive to be at the forefront of new media technology.",
+    colors: [],
+    tag: "web",
+  },
+  {
+    name: "BannerWave Component Library",
+    title: "A component library for building Animere",
+    tools: [
+      "React18",
+      "Storybook",
+      "Styled-Component",
+      "Webpack",
+      "Babel",
+      "TypeScript",
+      "NPM",
+    ],
+    images: [
+      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684837008/portfolio/screenshot-2023-05-21-at-172905-2_yho745.webp",
+      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684837009/portfolio/screenshot-2023-05-21-at-172851-2_mh8xgi.webp",
+      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684837009/portfolio/screenshot-2023-05-23-at-110631-2_fzcoec.webp",
+      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684837009/portfolio/screenshot-2023-05-21-at-173225-2_iwvd9i.webp",
+      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684837008/portfolio/screenshot-2023-05-23-at-110428-2_vc2j01.webp",
+      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684837008/portfolio/screenshot-2023-05-23-at-110428-2_vc2j01.webp",
+      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684837008/portfolio/screenshot-2023-05-23-at-110456-2_gl2v7k.webp",
+    ],
+    details:
+      "We’ve worked incredibly hard to build a talented, industry leading team of professionals. With a team of creative, strategist, business and development specialists, we consistently strive to be at the forefront of new media technology.",
+    colors: [],
+    tag: "web",
+  },
+  {
     name: "Clane Website",
     title: "A React Web Application",
     tools: ["React", "Redux", "TailwindCSS", "Styled-Component", "Axios"],
@@ -33,34 +85,7 @@ const PROJECTS: {
     colors: [],
     tag: "web",
   },
-  {
-    name: "Scandiweb",
-    title: "A React Web Application",
-    tools: ["React", "Redux", "TailwindCSS", "Styled-Component", "Axios"],
-    images: [
-      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684691043/portfolio/1_ugjdc6.webp",
-      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684691043/portfolio/2_kodziq.webp",
-      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684691043/portfolio/3_yrsqoy.webp",
-    ],
-    details:
-      "We’ve worked incredibly hard to build a talented, industry leading team of professionals. With a team of creative, strategist, business and development specialists, we consistently strive to be at the forefront of new media technology.",
-    colors: [],
-    tag: "web",
-  },
-  {
-    name: "First Portfolio",
-    title: "A React Web Application",
-    tools: ["React", "Redux", "TailwindCSS", "Styled-Component", "Axios"],
-    images: [
-      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684696710/portfolio/1_udec3m.webp",
-      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684696710/portfolio/2_jdkzz5.webp",
-      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684696710/portfolio/3_eiogxr.webp",
-    ],
-    details:
-      "We’ve worked incredibly hard to build a talented, industry leading team of professionals. With a team of creative, strategist, business and development specialists, we consistently strive to be at the forefront of new media technology.",
-    colors: [],
-    tag: "web",
-  },
+
   {
     name: "Rapid",
     title: "A React Native Merchant Mobile Application",
@@ -123,6 +148,34 @@ const PROJECTS: {
     details: "",
     tag: "mobile",
     colors: [],
+  },
+  {
+    name: "Scandiweb",
+    title: "A React Web Application",
+    tools: ["React", "Redux", "TailwindCSS", "Styled-Component", "Axios"],
+    images: [
+      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684691043/portfolio/1_ugjdc6.webp",
+      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684691043/portfolio/2_kodziq.webp",
+      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684691043/portfolio/3_yrsqoy.webp",
+    ],
+    details:
+      "We’ve worked incredibly hard to build a talented, industry leading team of professionals. With a team of creative, strategist, business and development specialists, we consistently strive to be at the forefront of new media technology.",
+    colors: [],
+    tag: "web",
+  },
+  {
+    name: "First Portfolio",
+    title: "A React Web Application",
+    tools: ["React", "Redux", "TailwindCSS", "Styled-Component", "Axios"],
+    images: [
+      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684696710/portfolio/1_udec3m.webp",
+      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684696710/portfolio/2_jdkzz5.webp",
+      "https://res.cloudinary.com/dwn5whgwh/image/upload/v1684696710/portfolio/3_eiogxr.webp",
+    ],
+    details:
+      "We’ve worked incredibly hard to build a talented, industry leading team of professionals. With a team of creative, strategist, business and development specialists, we consistently strive to be at the forefront of new media technology.",
+    colors: [],
+    tag: "web",
   },
   {
     name: "Tarvos",
